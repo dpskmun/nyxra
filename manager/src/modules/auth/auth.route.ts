7e734138040236b6a6139d53beeb2b17d2063f13
Hardik Gupta <hardikgupta0002@gmail.com>
2026-07-18 02:21:34 +0530

    fix: removed authRouter

--- manager/src/modules/auth/auth.route.ts
+++
@@ -1,5 +0,0 @@
-import Elysia from "elysia";
-
-export const authRouter = new Elysia({
-    prefix: "/",
-})
--- manager/src/versions/version.route.controller.ts
+++ manager/src/versions/version.route.controller.ts
@@ -1,5 +1,3 @@
 import { Elysia } from "elysia";
-import { authRouter } from "../modules/auth/auth.route";
 
 export const versionRouteController = new Elysia()
-.use(authRouter)
